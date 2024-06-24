7c8112e3db1ce9f2ff83f35cd57072eb2f2bfbd5
gabrielebova01 <bovaga01@gmail.com>
2024-06-24 11:59:28 +0200

    equazioni

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Questo repository è stato creato esclusivamente per scopi di analisi statistica del traffico (clones/visitors). I codici presenti sono stati scritti in passato senza alcuna pretesa di rigore.
 
 ## Codici brutti formattati belli:
-1. `01_somma.cpp` - Somma di due numeri interi (10/2017)
+01. `01_somma.cpp` - Somma di due numeri interi (2017-10-03).
+02. `02_Equazioni2.cpp` - Soluzioni di un'equazione di secondo grado (2017-10-17).
 
---
+++ src/02_Equazioni2.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void RisolviPrimoGrado(float c2, float c3) {
+    if ((c2 == 0) && (c3 == 0)) {
+        cout << "L'equazione e' indeterminata." << endl;
+    } else {
+        if (c2 == 0) {
+            cout << "L'equazione e' impossibile." << endl;
+        } else {
+            cout << "x = " << -c3 / c2 << endl;
+        }
+    }
+}  // RisolviPrimoGrado
+
+float CalcolaDelta(float c1, float c2, float c3) {
+    return c2 * c2 - 4 * c1 * c3;
+}  // CalcolaDelta
+
+void ScriviSoluzioni(float c1, float c2, float c3) {
+    float delta;   // discriminante
+    float x1, x2;  // soluzioni
+    delta = CalcolaDelta(c1, c2, c3);
+    cout << endl << "Delta = " << delta << endl;
+    if (delta < 0) {
+        cout << "L'equazione non ha soluzioni reali." << endl;
+    } else {
+        if (delta == 0) {
+            x1 = -c2 / (2 * c1);
+            cout << "x1 = x2 = " << x1 << endl;
+        } else {
+            x1 = (-c2 - sqrt(delta)) / (2 * c1);
+            x2 = (-c2 + sqrt(delta)) / (2 * c1);
+            cout << "x1 = " << x1 << endl;
+            cout << "x2 = " << x2 << endl;
+        }
+    }
+}  // ScriviSoluzioni
+
+// funzione principale
+int main() {
+    float a, b, c;  // coefficienti equazione
+    char risposta;
+    cout << "Questo programma risolve equazioni di secondo grado." << endl;
+    do {
+        cout << endl
+             << "Inserisci il coefficiente del termine di secondo grado:"
+             << endl;
+        cin >> a;
+        cout << "Inserisci il coefficiente del termine di primo grado:" << endl;
+        cin >> b;
+        cout << "Inserisci il termine noto:" << endl;
+        cin >> c;
+        if (a != 0) {
+            ScriviSoluzioni(a, b, c);
+        } else {
+            RisolviPrimoGrado(b, c);
+        }
+        cout << endl << "Vuoi continuare? <s/n>" << endl;
+        cin >> risposta;
+    } while (risposta == 'S' || risposta == 's');
+    return 0;
+}
+
