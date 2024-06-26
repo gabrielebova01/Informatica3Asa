c7a8e506046be75712a6728431ecb730761a2b5a
gabrielebova01 <bovaga01@gmail.com>
2024-06-26 11:38:18 +0200

    calendario

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Questo repository è stato creato esclusivamente per scopi di analisi statistica
 ## Codici brutti formattati belli:
 01. `01_somma.cpp` - Somma di due numeri interi (2017-10-03).
 02. `02_Equazioni2.cpp` - Soluzioni di un'equazione di secondo grado (2017-10-17).
-03. `.cpp` - (2017-).
+03. `03_.cpp` - (2017-).
 04. `04_Rettangolo.cpp` - Area, perimetro e diagonale di un'istanza della classe Rettangolo (2017-12-05).
+05. `05_.cpp` - (2018-).
+06. `06_calendario.cpp` - Calendario del mese di gennaio (2018-04-17).
+07. `07_.cpp` - (2018-).
 
---
+++ src/06_calendario.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void CaricaMese(string Gen[]);
+void StampaMese(string Gen[]);
+void CercaGiorno(string Gen[]);
+
+int main() {
+    string Gennaio[31];
+    CaricaMese(Gennaio);
+    char risp;
+    cout << "Calendario del mese di gennaio." << endl;
+    do {
+        cout << endl
+             << "Premi <s> per visualizzare il mese, <r> per cercare un giorno "
+                "o un altro tasto per uscire"
+             << endl;
+        cin >> risp;
+        if (risp == 's' || risp == 'S') {
+            StampaMese(Gennaio);
+        }
+        if (risp == 'r' || risp == 'R') {
+            CercaGiorno(Gennaio);
+        }
+    } while (risp == 's' || risp == 'S' || risp == 'r' || risp == 'R');
+    return 0;
+}
+
+void CaricaMese(string Gen[]) {
+    Gen[0] = "Lunedi'";
+    Gen[1] = "Martedi'";
+    Gen[2] = "Mercoledi'";
+    Gen[3] = "Giovedi'";
+    Gen[4] = "Venerdi'";
+    Gen[5] = "Sabato";
+    Gen[6] = "Domenica";
+    for (int Incremento = 0; Incremento < 7; Incremento++) {
+        for (int Giorno = 7 + Incremento; Giorno < 31; Giorno = Giorno + 7) {
+            Gen[Giorno] = Gen[Incremento];
+        }
+    }
+}
+
+void StampaMese(string Gen[]) {
+    cout << endl << "Il mese di gennaio 2018 e':" << endl;
+    for (int Giorno = 0; Giorno < 31; Giorno++) {
+        cout << Gen[Giorno] << " " << Giorno + 1 << endl;
+    }
+}
+
+void CercaGiorno(string Gen[]) {
+    int ricerca;
+    cout << endl << "Inserisci il numero del giorno che vuoi cercare:" << endl;
+    cin >> ricerca;
+    cout << "Il giorno " << ricerca << " e' " << Gen[ricerca - 1] << "."
+         << endl;
+}
+
